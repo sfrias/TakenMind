419f68c31e9f7aad24f697639b8aeea1b74ce68f
sfrias <sfrias1@gmail.com>
2019-07-21 18:43:39 +0200

    Updated methods

---
+++ 4_Methods/ConcatDfs1.py
@@ -0,0 +1,3 @@
+from pandas import Series, DataFrame, concat as pdconcat
+from numpy import random, concatenate as npconcat
+
--- 4_Methods/JoinDfs1.py
+++ 4_Methods/JoinDfs1.py
@@ -1,22 +1,25 @@
 import numpy as np
-from pandas import DataFrame, merge as Mergedf
+from pandas import DataFrame
 
 df1 = DataFrame({'data': range(5)},
-                index = ['O', 'U', 'L', 'O', 'U'])
+                index= ['O', 'U', 'L', 'O', 'U'])
 print '--- df1 ---'
 print df1
+print df1.index
 
 df2 = DataFrame({'profit': [10, 20, 20]},
                  index =['O', 'O', 'U'])
 print '--- df2 ---'
-print df2
+print df2.index
 
 print ' - df2 joined with df1 - '
-print df1.join(df2)
+df3 = df1.join(df2)
+print df3
+print df3.index
 print '==================='
-df3 = DataFrame({ 'ref1': ['A', 'A', 'O', 'O', 'A'],
-                  'ref2': [5, 10, 15, 20, 25],
-                  'ref3': range(5)})
+df3 = DataFrame({'ref1': ['A', 'A', 'O', 'O', 'A'],
+                 'ref2': [5, 10, 15, 20, 25],
+                 'ref3': range(5)})
 print '--- df3 ---'
 print df3
 print df3.index
@@ -42,5 +45,7 @@ df6 = DataFrame({ 'ref1': ['A', 'A', 'O', 'O', 'O'],
 print '--- df6 ---'
 print df6
 print df6.index
-print '---'
-print df3.join(df6, lsuffix='x', rsuffix= 'y')
+print '--- df4 joined df3 ---'
+df7 = df3.join(df6, lsuffix='x', rsuffix= 'y')
+print df7
+print df7.index
