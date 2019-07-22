99b5572e6230d4e30b948abcaf243bc805d7899f
sfrias <sfrias1@gmail.com>
2019-07-22 21:06:46 +0200

    Updated methods

--- 4_Methods/rshape1.py
+++ 4_Methods/rshape1.py
@@ -1,5 +1,5 @@
 from numpy import arange
-from pandas import Series, DataFrame, Index
+from pandas import Series, DataFrame, Index, concat
 
 # Create DataFrame
 df1 = DataFrame(arange(8).reshape(2, 4),
@@ -16,7 +16,7 @@ df2 = df1.stack('attributes')
 print '- stacked df1 for attributes -'
 print df2
 print df2.index
-# Unstac from serie to Dataframe
+# Unstack from serie to Dataframe
 undf2 = df2.unstack('attributes')
 print '--- unstacked df2 for attributes---'
 print undf2
@@ -26,3 +26,31 @@ undf3 = df2.unstack('cabs')
 print '--- unstacked df2 for cabs---'
 print undf3
 print undf3.index
+
+# Unstack from series to Dataframe
+s1 = Series([5, 10, 15], index=['c1', 'c2', 'c3'])
+print '--- s1 ---'
+print s1
+s2 = Series([15, 20, 25], index=['c2', 'c3', 'c4'])
+print '--- s2 ---'
+print s2
+
+s3 = concat([s1, s2], keys=['s1', 's2'])
+print '--- concat s1,s2 ---'
+print s3
+print s3.index
+
+df = s3.unstack(0)
+print '- unstack from Serie to DataFrame -'
+print '- First index=0 as columns'
+print df
+print df.index
+
+df = s3.unstack(1)
+print '- unstack from Serie to DataFrame -'
+print '- Second index=1 as columns'
+print df
+print df.index
+
+# When we need to retrieve NaN for non values
+print df.stack(dropna=False)
