7787adc0eaa4c0cd07f2e78cfc859699b36a5e67
sfrias <sfrias1@gmail.com>
2019-07-22 02:56:32 +0200

    Updated methods

--- 4_Methods/ConcatDfs1.py
+++ 4_Methods/ConcatDfs1.py
@@ -4,8 +4,8 @@ from numpy import arange, random,\
     concatenate as npconcat
 
 # Create numpy arrays
-a1 = random.randn(25).reshape(5,5)
-b1 = arange(25).reshape(5,5)
+a1 = random.randn(25).reshape(5, 5)
+b1 = arange(25).reshape(5, 5)
 
 print '--- a1 ---'
 print a1
@@ -14,10 +14,10 @@ print b1
 
 # Concatenate/link numpy arrays
 print '--- Concatenate a1, b1 rows(axis=0)'
-print npconcat([a1,b1], axis=0)
+print npconcat([a1, b1], axis=0)
 
 print '--- Concatenate a1, b1 columns(axis=1)'
-print npconcat([a1,b1], axis=1)
+print npconcat([a1, b1], axis=1)
 
 # Create Series
 s1 = Series([100, 200, 300], index=['A', 'B', 'C'])
@@ -28,20 +28,29 @@ print '--- s2 ---'
 print s2
 # Concatenate/link Series
 print '--- concat(s1,s2) axis = 0---'
-print pdconcat([s1,s2])
-print '--- concat(s2,s1) axis = 0---'
-print pdconcat([s2,s1])
+print pdconcat([s1, s2])
+print '--- concat(s2, s1) axis = 0---'
+print pdconcat([s2, s1])
 print '--- series concat(s1,s2) axis = 0---'
-s = pdconcat([s1,s2], axis= 0,
-             keys =['s1', 's2'],
+s = pdconcat([s1, s2], axis=0,
+             keys=['s1', 's2'],
              names=['idx_s', 'idx'])
 print s
 
 print '--- series concat(s1,s2) axis = 1---'
-s = pdconcat([s1,s2], axis= 1, sort= False,
-             keys =['s1', 's2'],
+s = pdconcat([s1, s2], axis=1, sort=False,
+             keys=['s1', 's2'],
              names=['idx'])
 print s
-# Create Dataframes
 
+# Create Dataframes
+df1 = DataFrame(random.randn(4, 3),
+                columns=['A', 'B', 'C'])
+df2 = DataFrame(random.randn(3, 3),
+                columns=['B', 'D', 'A'])
+print '--- df1 ---'
+print df1
+
+print '--- df2 ---'
+print df2
 # Concatenate/link Dataframes
