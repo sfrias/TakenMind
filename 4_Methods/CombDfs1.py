4c4451f657a5cee569cb776df4c780062594967e
sfrias <sfrias1@gmail.com>
2019-07-22 17:03:52 +0200

    Updated methods

--- 4_Methods/CombDfs1.py
+++ 4_Methods/CombDfs1.py
@@ -1,11 +1,14 @@
+
+
 from numpy import nan, float64, arange, where
 from pandas import Series, DataFrame, isnull
 
-s1=Series([5, nan, 6, nan], index=['A', 'B', 'C', 'D'])
+# Create Series
+s1 = Series([5, nan, 6, nan], index=['A', 'B', 'C', 'D'])
 print '--- s1 ---'
 print s1
 
-s2=Series(arange(4), dtype=float64, index=s1.index)
+s2 = Series(arange(4), dtype=float64, index=s1.index)
 print '--- s2 ---'
 print s2
 
@@ -19,3 +22,20 @@ print s3
 s4 = s1.combine_first(s2)
 print '---- Same with combine_first method ---'
 print s4
+print '========='
+# Create Dataframes
+
+df1 = DataFrame({'col1': [5, nan, 15],
+                 'col2': [20, 25, nan],
+                 'col3': [nan, nan, 35]})
+print '--- df1 ---'
+print df1
+
+df2 = DataFrame({'col1': [0, 10, 15],
+                 'col3': [10, 20, 30]})
+print '--- df2 ---'
+print df2
+
+# Combine Dataframes
+print '- df1 combine_first df2'
+print df1.combine_first(df2)
