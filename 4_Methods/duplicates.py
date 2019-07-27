6623b9451d4ec17c44aeea8bdd4403dee9f50ac7
sfrias <sfrias1@gmail.com>
2019-07-27 18:52:40 +0200

    Updated methods

--- 4_Methods/duplicates.py
+++ 4_Methods/duplicates.py
@@ -13,10 +13,14 @@ print df.drop_duplicates()
 
 print'- Duplicated for col1 coincidence -'
 print df.duplicated(['col1'])
-print '- Dropped duplicates, col1 -'
-print df.drop_duplicates(['col1'])
+print '- Dropped duplicates keeping first, col1 -'
+print df.drop_duplicates(['col1'], keep='first')
+print '- Dropped duplicates keeping last, col1 -'
+print df.drop_duplicates(['col1'], keep='last')
 
 print'- Duplicated for col2 coincidence -'
 print df.duplicated(['col2'])
-print '- Dropped duplicates, col2 -'
-print df.drop_duplicates(['col2'])
+print '- Dropped duplicates keeping first, col2 -'
+print df.drop_duplicates(['col2'], keep='first')
+print '- Dropped duplicates keeping last, col2 -'
+print df.drop_duplicates(['col2'], keep='last')
